4cb80e9b65880e6b6c4f0fffce8a6198ef775769
Slothing <seth.r.martineau@gmail.com>
2020-11-17 08:37:51 -0700

    got server working with routes

--- Develop/server.js
+++ Develop/server.js
@@ -7,13 +7,13 @@ const PORT = process.env.PORT || 8080;
 
 app.use(morgan("dev"));
 
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 
-// require("./routes/apiRoutes")(app);
-// require("./routes/htmlRoutes")(app);
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
 
-app.listen(PORT,function(){ 
-    console.log("Local host on Port " + PORT);
+app.listen(PORT, function () {
+  console.log("Local host on Port " + PORT);
 });
