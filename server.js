64bc9b2558361f24f81048c784bed37a53dfdcd4
Slothing <seth.r.martineau@gmail.com>
2020-11-19 06:56:13 -0700

    deploying to heroku

--- server.js
+++ server.js
@@ -12,11 +12,15 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 
-var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/workoutDB";
-mongoose.connect(MONGODB_URI,{  
-    useNewUrlParser:true,
-    useFindAndModify:false
-})
+mongoose.connect(
+  process.env.MONGODB_URI || 'mongodb://localhost/workoutDB',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  }
+);
 
 require("./routes/apiRoutes")(app);
 require("./routes/htmlRoutes")(app);
