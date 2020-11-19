17a65efab5b48d5959ed745398c800f0ef6da27c
Slothing <seth.r.martineau@gmail.com>
2020-11-19 06:38:08 -0700

    got db connected properly

--- Develop/server.js
+++ Develop/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const morgan = require("morgan");
 const mongoose = require("mongoose");
 
+
 const app = express();
 const PORT = process.env.PORT || 8080;
 
@@ -10,6 +11,12 @@ app.use(morgan("dev"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
+
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/workoutDB";
+mongoose.connect(MONGODB_URI,{  
+    useNewUrlParser:true,
+    useFindAndModify:false
+})
 
 require("./routes/apiRoutes")(app);
 require("./routes/htmlRoutes")(app);
